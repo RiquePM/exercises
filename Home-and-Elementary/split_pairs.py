dd75e325996f7fa852024f45230008833425fd25
Henrique Pinheiro <73786083+RiquePM@users.noreply.github.com>
2021-06-02 17:40:14 -0300

    Update split_pairs.py

--- Home-and-Elementary/split_pairs.py
+++ Home-and-Elementary/split_pairs.py
@@ -1,5 +1,5 @@
 #--------------------------------------------#
-#				 My Solution 				 #
+#		My Solution		     #
 #--------------------------------------------#
 def split_pairs(a):
 	if len(a) % 2 == 0:
@@ -12,7 +12,7 @@ def split_pairs(a):
 	return pairs
 
 #--------------------------------------------#
-#			   Better Solutions			     #
+#	      Better Solutions	     	     #
 #--------------------------------------------#
 
 #def split_pairs(a):
@@ -23,26 +23,26 @@ def split_pairs(a):
 		#a = a + '_'
 	#return [a[i:i + 2] for i in range(0, len(a), 2)]	
 
-#from textwrap import wrap → olhar a biblioteca texwrap
+#from textwrap import wrap → estudar a biblioteca texwrap
 #def split_pairs(a):
 	#a = a + '_' if len(a) % 2 else a
 	#return wrap(a, 2)
 
-#import itertools, operator → olhar a biblioteca itertools
+#import itertools, operator → estudar a biblioteca itertools
 #def split_pairs(a):
 	#it = itertools.chain(a, '_')
 	#return map(operator.add, it, it) → olhar a função map
 
 #def split_pairs(a):
-	#return [ch1+ch2 for ch1,ch2 in zip(a[::2],a[1::2]+'_')] → olhar a função zip 
+	#return [ch1+ch2 for ch1,ch2 in zip(a[::2],a[1::2]+'_')] → estudar a função zip 
 
 #def split_pairs(a):
-	#return [a[i-1]+c for i,c in enumerate(a+'_'*(len(a)%2)) if i%2] → olhar a função enumerate
+	#return [a[i-1]+c for i,c in enumerate(a+'_'*(len(a)%2)) if i%2] → estdudar a função enumerate
 
 #split_pairs = lambda a: [x + y for x, y in zip(a[::2], a[1::2] + '_')]
 
 #--------------------------------------------#
-#					Test					 #
+#		    Test		     #
 #--------------------------------------------#
 
 a = split_pairs("abcd")
@@ -50,5 +50,3 @@ b = split_pairs("abc")
 c = split_pairs("")
 d = split_pairs("a")
 print(a, b, c, d, sep = '\n')
-		
-
